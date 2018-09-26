a03a6c363de76c89dd67b66ae3126d85e27ed1b9
Luky Rahman <lukyrahman68@gmail.com>
2018-09-26 13:06:28 +0700

    add model role-AdminUserController and some database

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('', function(){
+
+// });
+
+Route::resource('admin/users', 'AdminUsersController');
